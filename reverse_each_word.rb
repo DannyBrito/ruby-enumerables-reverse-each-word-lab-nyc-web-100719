f14bc7f3518813660248a1fee9a273d406100aea
Danny Brito <britodanny1997@gmail.com>
2019-09-28 16:50:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  array.each{|word| word.reverse}
-  array.join(" ")
+  result_string = ""
+  array.each{|word| string += word.reverse}
+  result_string
 end 
 
 reverse_each_word("Helo there, and how are you?")
